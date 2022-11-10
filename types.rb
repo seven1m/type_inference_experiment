d1a955a464239bd62679e71b2e81e8574cd415c6
Tim Morgan <tim@timmorgan.org>
2022-11-09 19:41:42 -0600

    VM: fix built-in method call and add if

--- types.rb
+++ types.rb
@@ -93,6 +93,8 @@ class Compiler
       @out << Instruction.new(:push_int, node: node, arg: node[1])
     when :str
       @out << Instruction.new(:push_str, node: node, arg: node[1])
+    when :nil
+      @out << Instruction.new(:push_nil, node: node)
     else
       raise "unknown node: #{node.inspect}"
     end
@@ -598,15 +600,15 @@ class VM
   def initialize(code)
     @code = code
     @instructions = Compiler.new(code).compile
-    @instructions_meta = TypeInferrer.new(@instructions, code: code).infer
+    @typed_instructions = TypeInferrer.new(@instructions, code: code).infer
     @scope = [{ vars: {}, stack: [] }]
     @methods = {}
   end
 
   def run
     index = 0
-    while index < @instructions_meta.size
-      meta = @instructions_meta[index]
+    while index < @typed_instructions.size
+      meta = @typed_instructions[index]
       instruction = meta.fetch(:instruction)
 
       case instruction.type
@@ -618,9 +620,7 @@ class VM
         stack.push(vars[instruction.arg])
       when :def
         @methods[instruction.arg] = index + 1
-        until @instructions_meta[index][:instruction].type == :end_def
-          index += 1
-        end
+        index += 1 until @typed_instructions[index][:instruction].type == :end_def
       when :end_def
         frame = @scope.pop
         stack << frame[:stack].pop
@@ -628,9 +628,51 @@ class VM
       when :push_nil
         stack << nil
       when :send
-        @scope << { vars: {}, stack: [], return: index }
-        index = @methods[instruction.arg]
-        next
+        args = []
+        instruction.extra_arg.times { args << stack.pop }
+        receiver = stack.pop
+        name = instruction.arg
+        if receiver
+          unless receiver.respond_to?(name)
+            raise MethodError, "#{receiver.class} has no method `#{name}'"
+          end
+          stack << receiver.send(name, *args)
+        else
+          @scope << { vars: {}, stack: [], return: index, args: args }
+          index = @methods.fetch(name)
+          next
+        end
+      when :push_arg
+        stack << args.pop
+      when :if
+        condition = stack.pop
+        depth = 0
+        unless condition
+          index += 1
+          until depth.zero? && @typed_instructions[index][:instruction].type == :else
+            case @typed_instructions[index][:instruction].type
+            when :if
+              depth += 1
+            when :end_if
+              depth -= 1
+            end
+            index += 1
+          end
+        end
+      when :else
+        depth = 0
+        index += 1
+        until depth.zero? && @typed_instructions[index][:instruction].type == :end_if
+          case @typed_instructions[index][:instruction].type
+          when :if
+            depth += 1
+          when :end_if
+            depth -= 1
+          end
+          index += 1
+        end
+      when :end_if
+        :noop
       else
         raise "unknown VM instruction: #{instruction.inspect}"
       end
@@ -649,6 +691,10 @@ class VM
   def vars
     @scope.last.fetch(:vars)
   end
+
+  def args
+    @scope.last.fetch(:args)
+  end
 end
 
 describe 'VM' do
@@ -661,11 +707,29 @@ describe 'VM' do
   end
 
   it 'can define and call methods' do
-    skip
     code = <<~CODE
       def foo(x); x + 1; end
       foo(2)
     CODE
     expect(run_vm(code)).must_equal 3
+  end
+
+  it 'can branch with if' do
+    code = <<~CODE
+      if nil
+        if 1
+          2
+        else
+          3
+        end
+      else     # <-- take this branch
+        if 4
+          5    # <-- final result
+        else
+          6
+        end
+      end
+    CODE
+    expect(run_vm(code)).must_equal 5
   end
 end
